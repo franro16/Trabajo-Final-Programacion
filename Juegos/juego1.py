887779a97e72192a0bc22f2b3352a7c175d74c44
franro16 <francoisaiasromero.16@gmail.com>
2024-10-16 00:34:43 -0300

    Problemas con el iniciado del juego
    Una vez el usuario seleccionaba el juego no redireccionaba al archivo juego1.py por lo que se llego a la solucion de agregar  def juego_iniciado():
        juego_adivina_la_palabra_oculta()
    
    if __name__ == "__main__":
        juego_iniciado()  para asi llamar al juego una vez se seleccionado el juego

--- Juegos/juego1.py
+++ Juegos/juego1.py
@@ -1,63 +1,57 @@
 import random
 
-
 def obtener_palabra_aleatoria():
-    palabras = ["computadora", "programacion", "universidad"]
+    palabras = ["computadora"]
     return random.choice(palabras)
 
-def mostrar_tablero(palabra_oculta,intentos):
+def mostrar_tablero(palabra_oculta, intentos):
     print("************")
     print("Palabra:", " ".join(palabra_oculta))
-    print("Intentos Restantes:", intentos, "  ","♡"*intentos)
+    print("Intentos Restantes:", intentos, "  ", "♡" * intentos)
     print("************")
 
-def adivinar_letra(palabra,palabra_oculta,letra):
-        aciertos = 0
-        for i in range(len(palabra)):
-            if palabra[i] == letra:
-                palabra_oculta[i] = letra
-                aciertos +=1
-        return aciertos
+def adivinar_letra(palabra, palabra_oculta, letra):
+    aciertos = 0
+    for i in range(len(palabra)):
+        if palabra[i] == letra:
+            palabra_oculta[i] = letra
+            aciertos += 1
+    return aciertos
 
 def juego_adivina_la_palabra_oculta():
     palabra = obtener_palabra_aleatoria()
-    palabra_oculta = ["_" for _ in palabra];
+    palabra_oculta = ["_" for _ in palabra]
     intentos = 7
-    letras_uasadas = []
+    letras_usadas = []
 
     while True:
-
         mostrar_tablero(palabra_oculta, intentos)
 
         if "_" not in palabra_oculta:
-            print("muy bien, ¡Ganaste!, la palabra era:", " ".join(palabra))
+            print("¡Muy bien, ganaste! La palabra era:", " ".join(palabra))
             break
         if intentos == 0:
-            print("has perdido, la palabra oculta era:", " ".join(palabra))
+            print("Has perdido. La palabra oculta era:", " ".join(palabra))
             break
 
         letra = input("INGRESE UNA LETRA: ").lower()
 
-        if letra in letras_uasadas:
-            print("ya has utilizado esta letra, por favor ingrese ortra")
-           
+        if letra in letras_usadas:
+            print("Ya has utilizado esta letra, por favor ingrese otra")
             continue
 
-        letras_uasadas.append(letra)
-        
-    
-        aciertos = adivinar_letra(palabra,palabra_oculta,letra) 
+        letras_usadas.append(letra)
+
+        aciertos = adivinar_letra(palabra, palabra_oculta, letra)
         if aciertos > 0:
-            print("adivinaste", aciertos, "letra")
+            print("Adivinaste", aciertos, "letra(s).")
         else:
-            print("la letra no pertenece a la palabra oculta")
-            intentos-=1
+            print("La letra no pertenece a la palabra oculta.")
+            intentos -= 1
             print("************")
-          
-def main():
-    while True:
-        juego_adivina_la_palabra_oculta()
-        jugar_de_nuevo = input("¿Desea volver a jugar? (si/no) " ).lower()
-        if jugar_de_nuevo != 'si':
-           print("gracias por jugar. ¡Hasta la proxima!")
-           break
+
+def juego_iniciado():  
+    juego_adivina_la_palabra_oculta()
+
+if __name__ == "__main__":
+    juego_iniciado()  
