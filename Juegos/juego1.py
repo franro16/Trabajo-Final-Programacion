be61c884752a3af29566c67e855c41514716f12c
franro16 <francoisaiasromero.16@gmail.com>
2024-10-16 00:25:46 -0300

    Se comienza las pruebas con el primer juego

--- Juegos/juego1.py
+++ Juegos/juego1.py
@@ -1,2 +1,63 @@
-def juego_iniciado():
-    print("Este es el juego nro 1")
+import random
+
+
+def obtener_palabra_aleatoria():
+    palabras = ["computadora", "programacion", "universidad"]
+    return random.choice(palabras)
+
+def mostrar_tablero(palabra_oculta,intentos):
+    print("************")
+    print("Palabra:", " ".join(palabra_oculta))
+    print("Intentos Restantes:", intentos, "  ","♡"*intentos)
+    print("************")
+
+def adivinar_letra(palabra,palabra_oculta,letra):
+        aciertos = 0
+        for i in range(len(palabra)):
+            if palabra[i] == letra:
+                palabra_oculta[i] = letra
+                aciertos +=1
+        return aciertos
+
+def juego_adivina_la_palabra_oculta():
+    palabra = obtener_palabra_aleatoria()
+    palabra_oculta = ["_" for _ in palabra];
+    intentos = 7
+    letras_uasadas = []
+
+    while True:
+
+        mostrar_tablero(palabra_oculta, intentos)
+
+        if "_" not in palabra_oculta:
+            print("muy bien, ¡Ganaste!, la palabra era:", " ".join(palabra))
+            break
+        if intentos == 0:
+            print("has perdido, la palabra oculta era:", " ".join(palabra))
+            break
+
+        letra = input("INGRESE UNA LETRA: ").lower()
+
+        if letra in letras_uasadas:
+            print("ya has utilizado esta letra, por favor ingrese ortra")
+           
+            continue
+
+        letras_uasadas.append(letra)
+        
+    
+        aciertos = adivinar_letra(palabra,palabra_oculta,letra) 
+        if aciertos > 0:
+            print("adivinaste", aciertos, "letra")
+        else:
+            print("la letra no pertenece a la palabra oculta")
+            intentos-=1
+            print("************")
+          
+def main():
+    while True:
+        juego_adivina_la_palabra_oculta()
+        jugar_de_nuevo = input("¿Desea volver a jugar? (si/no) " ).lower()
+        if jugar_de_nuevo != 'si':
+           print("gracias por jugar. ¡Hasta la proxima!")
+           break
--- Menu.py
+++ Menu.py
@@ -5,36 +5,37 @@ from Juegos import juego4
 import time
 
 def menu():
-    print("******************************")
-    print("        ¡Bienvenido/a!")
-    print("******************************")
-    time.sleep(1.5)
-    print("Ingresa un numero para elegir un juego")
-    time.sleep(1.5)
-    print("1---       Adivina la Palabra      ---1")
-    print("2--- Tal juego                     ---2")
-    print("3--- Tal juego                     ---3")
-    print("4--- Tal juego                     ---4")
-    opcion = input("Que queres jugar?, Ingresa el numero: ")
+    while True:
+        print("******************************")
+        print("        ¡Bienvenido/a!")
+        print("******************************")
+        time.sleep(1.5)
+        print("Ingresa un numero para elegir un juego")
+        time.sleep(1.5)
+        print("1---       Adivina la Palabra      ---1")
+        print("2--- Tal juego                     ---2")
+        print("3--- Tal juego                     ---3")
+        print("4--- Tal juego                     ---4")
+        opcion = input("Que queres jugar?, Ingresa el numero: ")
 
-    if opcion == "1":
-        print("---Elegiste Adivina la palabra---")
-        time.sleep(2)
-        juego1.juego_iniciado()
-    elif opcion == "2":
-        print("---Elegiste Adivina la palabra---")
-        time.sleep(2)
-        juego2.juego_iniciado()
-    elif opcion == "3":
-        print("---Elegiste Adivina la palabra---")
-        time.sleep(2)
-        juego3.juego_iniciado()
-    elif opcion == "4":
-        print("---Elegiste Adivina la palabra---")
-        time.sleep(2)
-        juego4.juego_iniciado()
-    else:
-        print("Opción no válida")
+        if opcion == "1":
+            print("---Elegiste Adivina la palabra---")
+            time.sleep(2)
+            juego1.juego_iniciado()
+        elif opcion == "2":
+            print("---Elegiste Tal juego 2---")
+            time.sleep(2)
+            juego2.juego_iniciado()
+        elif opcion == "3":
+            print("---Elegiste Tal juego 3---")
+            time.sleep(2)
+            juego3.juego_iniciado()
+        elif opcion == "4":
+            print("---Elegiste Tal juego 4---")
+            time.sleep(2)
+            juego4.juego_iniciado()
+        else:
+            print("Opción no válida")
 
 if __name__ == "__main__":
     menu()  
