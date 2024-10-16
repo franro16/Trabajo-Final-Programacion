44607a16aafa7e0097247e1e1e44bb95dfaae703
franro16 <francoisaiasromero.16@gmail.com>
2024-10-15 23:39:08 -0300

    Pequeñas modificaciones esteticas
    Se decidio hacer modificaciones en el menu para embellecer y hacer mas amena la experiencia UI

--- Menu.py
+++ Menu.py
@@ -2,23 +2,36 @@ from Juegos import juego1
 from Juegos import juego2
 from Juegos import juego3
 from Juegos import juego4
+import time
 
 def menu():
-    input("¡Bienvenido/a!")
+    print("******************************")
+    print("       ¡Bienvenido/a!")
+    print("******************************")
+    time.sleep(2)
     print("Ingresa un numero para elegir un juego: ")
-    print("1--- Adivina la Palabra ---1")
-    print("2--- Tal juego 2---2")
-    print("3--- Tal juego 3 ---3")
-    print("4--- Tal juego 4 ---4")
+    time.sleep(2)
+    print("1--- Adivina la Palabra            ---1")
+    print("2--- Tal juego                     ---2")
+    print("3--- Tal juego                     ---3")
+    print("4--- Tal juego                     ---4")
     opcion = input("Que queres jugar?, Ingresa el numero: ")
 
     if opcion == "1":
+        print("---Elegiste Adivina la palabra---")
+        time.sleep(2)
         juego1.juego_iniciado()
     elif opcion == "2":
+        print("---Elegiste Adivina la palabra---")
+        time.sleep(2)
         juego2.juego_iniciado()
     elif opcion == "3":
-        juego3.juego_iniciado() 
+        print("---Elegiste Adivina la palabra---")
+        time.sleep(2)
+        juego3.juego_iniciado()
     elif opcion == "4":
+        print("---Elegiste Adivina la palabra---")
+        time.sleep(2)
         juego4.juego_iniciado()
     else:
         print("Opción no válida")
