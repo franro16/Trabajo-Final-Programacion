26708441bb14243659c808be1e5091a56b0dd4a0
franro16 <francoisaiasromero.16@gmail.com>
2024-10-25 14:06:30 -0300

    .

--- Juegos/juego4.py
+++ Juegos/juego4.py
@@ -1,6 +1,11 @@
+import time
 def juego3():
     print ("hola mundo")
-
+    jugar_de_nuevo = input("¿Desea volver a jugar? (si/no) ").lower()
+    if jugar_de_nuevo != 'si':
+        print("Gracias por jugar. ¡Hasta la proxima!")
+        time.sleep(2)
+            
 def juego_iniciado():  
     juego3()
 
