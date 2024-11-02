fedaab0b3f15ab49bfeda4cf33ce14b56dbcd1f4
Aaagustin <122519354+Aaagustin@users.noreply.github.com>
2024-11-02 02:02:14 -0300

    Update juego1.py
    comentarios en el juego de adivinar la palabra

--- Main/juego1.py
+++ Main/juego1.py
@@ -1,28 +1,29 @@
 import random
 import time
 def juego1():
-    while True:
+    while True: #Bucle que nos permite volver a jugar
         
         palabras = ["computadora", "programacion", "auto", "perro", "arbol", "estrella"]
-        palabra = random.choice(palabras)
-        palabra_oculta = ["_" for _ in palabra]
+        palabra = random.choice(palabras) #Selecciona una palabra aleatoria
+        palabra_oculta = ["_" for _ in palabra] #Pasa la palabra a "_"
         intentos = 2
-        letras_usadas = []
+        letras_usadas = [] #Lista para almacenar las letras que ingrese el jugador
 
         while True:
             print("*******************************")
-            print("Palabra:", " ".join(palabra_oculta))
+            print("Palabra:", " ".join(palabra_oculta)) #Muestra la palabra oculta con espacio de por medio
             print("Intentos Restantes:", intentos, "  ", "♡" * intentos)
             print("*******************************")
 
             if "_" not in palabra_oculta:
                 print("¡Felicitaciones! Has ganado, la palabra oculta era:", " ".join(palabra))
                 break
-            if intentos == 0:
+            if intentos == 0: 
                 print("Has perdido, la palabra oculta era:", " ".join(palabra))
                 break
-
-            letra = input("INGRESE UNA LETRA: ").lower()
+                #El bucle for es para saber si no quedan mas "_" para saber si el jugador gano.
+                #El if es para ver si el jugador perdio
+            letra = input("INGRESE UNA LETRA: ").lower() #La letra que pide la pasa a minusculas
         
             if not letra.isalpha() or len(letra) != 1:
                 print("Por favor, ingresa solo una letra.")
@@ -32,15 +33,16 @@ def juego1():
                 print("Ya has utilizado esta letra, por favor ingrese otra.")
                 continue
 
-            letras_usadas.append(letra)
+            letras_usadas.append(letra)  
+            #agrega la letra ingresada por el jugador a la lista de letras usadas 
 
-            if letra in palabra:
+            if letra in palabra: #Para verificar si la letra que ingreso el jugador esta en la palabra
                 for i in range(len(palabra)):
                     if palabra[i] == letra:
-                        palabra_oculta[i] = letra
+                        palabra_oculta[i] = letra #Reemplaza "_" por la letra correcta
                 print("¡Adivinaste una letra!")
             else:
-                print("La letra no pertenece a la palabra oculta.")
+                print("La letra no pertenece a la palabra oculta.") #Si la letra no esta en la palabra se resta una vida
                 intentos -= 1
                 print("*******************************")
 
@@ -49,9 +51,10 @@ def juego1():
             print("Gracias por jugar. ¡Hasta la proxima!")
             time.sleep(2)
             break 
+            #Esto es para saber si el jugador desea volver a jugar
 
-def juego_iniciado():  
-    juego1()
+def juego_iniciado():  #Funcion para iniciar el juego
+    juego1() #Llama a la funcion juego1 para empezar 
 
 if __name__ == "__main__":
     juego_iniciado()  
